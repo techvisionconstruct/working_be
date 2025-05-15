4a5e05652bc8f46768d0ff13b0823b48826ff839
Arnaz <tech@avorino.com>
2025-05-16 02:29:11 +0800

    fix 3

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,4 @@
-#!/bin/bash
 echo "BUILD START"
-python3.12 -m pip install --upgrade pip
-python3.12 -m pip install -r requirements/prod.txt --target ./.vercel/python
-python manage.py collectstatic --noinput --clear
-echo "BUILD END"
+ python3.12 -m pip install -r ./requirements/prod.txt --target ./.vercel/python
+ python3.12 manage.py collectstatic --noinput --clear
+ echo "BUILD END"
