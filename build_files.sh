64e3dd88b55b616e515c0f370c7d864bafbe4fbd
Arnaz <tech@avorino.com>
2025-05-16 03:22:45 +0800

    changed 6

--- build_files.sh
+++ build_files.sh
@@ -2,16 +2,16 @@
 echo "BUILD START"
 
 # Install dependencies to Vercel's expected location
-python -m pip install --upgrade pip
-python -m pip install -r ./requirements/prod.txt --target ./.vercel/python
+python3.12 -m pip install --upgrade pip
+python3.12  -m pip install -r ./requirements/prod.txt --target ./.vercel/python
 
 # Set Python path (crucial for Django to find packages)
 export PYTHONPATH=$PYTHONPATH:$(pwd)/.vercel/python
 
 # Verify Django is installed
-python -c "import django; print(django.__version__)"
+python3.12  -c "import django; print(django.__version__)"
 
 # Collect static files
-python manage.py collectstatic --noinput --clear
+python3.12 manage.py collectstatic --noinput --clear
 
 echo "BUILD END"
