0720ed76d86e13e6e98d67c9d147033334eec083
Arnaz <tech@avorino.com>
2025-05-16 02:57:22 +0800

    added 2

--- build_files.sh
+++ build_files.sh
@@ -6,10 +6,10 @@ python3.12 -m pip install -r ./requirements/prod.txt --target ./.vercel/python
 
 # Ensure Python can find the packages
 export PYTHONPATH=$PYTHONPATH:$(pwd)/.vercel/python
-print("PYTHONPATH: $PYTHONPATH")
+
 # Collect static files with correct Python path
 python3.12 -m pip install --upgrade pip
 python3.12 manage.py collectstatic --noinput --clear
-# pip list
+pip list
 
 echo "BUILD END"
